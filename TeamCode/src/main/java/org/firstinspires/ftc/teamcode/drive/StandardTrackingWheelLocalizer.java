c92e889b6835e530c1f2d5090d473613d27806ae
DecebalTech <mihutmatei@gmail.com>
2022-03-21 15:06:41 +0200

    da

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -30,8 +30,9 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double TICKS_PER_REV =8192;
     public static double WHEEL_RADIUS = 0.688; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
+    //public static double LATERAL_DISTANCE = 11.22; // in; distance between the left and right wheels
+    public static double LATERAL_DISTANCE = 11.20; // in; distance between the left and right wheels
 
-    public static double LATERAL_DISTANCE = 11.22; // in; distance between the left and right wheels
     public static double FORWARD_OFFSET = 5.12; // in; offset of the lateral wheel
     //schimbat x si y multiplier conform masuratori si formula x mult =dist masurata/distanta telemetrie
     public static double X_MULTIPLIER = 1.0209290454; // Multiplier in the X direction
